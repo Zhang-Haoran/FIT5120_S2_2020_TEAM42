3d1ea1c207319cdf208af5a864913ff33b902ba5
Haoran Zhang <hzha278@student.monash.edu>
2020-08-21 18:55:56 +1000

    Add project files.

---
+++ FIT5120_S2_2020_TEAM42/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5120_S2_2020_TEAM42.MvcApplication" Language="C#" %>
---
+++ FIT5120_S2_2020_TEAM42/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5120_S2_2020_TEAM42.Startup))]
+namespace FIT5120_S2_2020_TEAM42
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
